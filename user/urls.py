1d6b5fd6899ad936c43d93c9c3337fabb207bd4d
di-m-dy <dimdy11.11@gmail.com>
2024-09-29 11:29:21 +0200

    home_task#22_2 add reset password

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from user.views import RegisterCreateView, email_verification
+from user.views import RegisterCreateView, email_verification, reset_password
 
 app_name = 'user'
 
@@ -9,5 +9,6 @@ urlpatterns = [
     path('', LoginView.as_view(template_name='user/login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', RegisterCreateView.as_view(), name='register'),
-    path('check_email/<str:token>/', email_verification, name='email_verification')
+    path('check_email/<str:token>/', email_verification, name='email_verification'),
+    path('reset_password', reset_password, name='reset_password')
 ]
--- user/views.py
+++ user/views.py
@@ -1,4 +1,6 @@
 import secrets
+import string
+import random
 
 from django.core.mail import send_mail
 from django.urls import reverse_lazy, reverse
@@ -39,3 +41,29 @@ def email_verification(request, token):
     user.is_active = True
     user.save()
     return redirect(reverse('user:login'))
+
+
+def reset_password(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+
+        check = User.objects.filter(email=email).exists()
+        if check:
+            user = User.objects.get(email=email)
+            letters = string.ascii_letters  # Заглавные и строчные буквы
+            digits = string.digits  # Цифры
+            special_chars = string.punctuation  # Специальные символы
+            all_chars = letters + digits + special_chars
+            password = ''.join(random.choice(all_chars) for _ in range(8))
+            user.set_password(password)
+            user.save()
+            send_mail(
+                subject="Сброс пароля",
+                message=f"Пароль сброшен. Ваш новый ременный пароль: {password}",
+                from_email=EMAIL_HOST_USER,
+                recipient_list=[user.email]
+            )
+            return render(request, 'user/success_reset_password.html')
+        else:
+            return render(request, 'user/error_reset_password.html')
+    return render(request, 'user/reset_password.html')
